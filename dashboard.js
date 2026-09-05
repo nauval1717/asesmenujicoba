94c31c512f1c9d873617942404b684dd15d19d2f
nauval1717 <ninikserianu@gmail.com>
2026-09-05 12:37:27 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,34 @@
 import { supabase } from './supabase.js';
 
 const logoutButton = document.getElementById('logoutButton');
+const teacherName = document.getElementById('teacherName');
 
+
+// CEK LOGIN
+const { data: { user } } = await supabase.auth.getUser();
+
+if (!user) {
+  window.location.href = 'index.html';
+}
+
+
+// AMBIL DATA PROFIL GURU
+if (user) {
+
+  const { data: profile, error } = await supabase
+    .from('profiles')
+    .select('full_name')
+    .eq('id', user.id)
+    .single();
+
+  if (!error && profile) {
+    teacherName.textContent = profile.full_name;
+  }
+
+}
+
+
+// LOGOUT
 logoutButton.addEventListener('click', async () => {
 
   const { error } = await supabase.auth.signOut();
